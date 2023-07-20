ef3b520c4bd3883cfc9c19c8e56cbca6e093b183
Swapnil7156 <swapnilshinde7156@gmail.com>
2023-07-20 23:20:53 +0530

    Code added

--- arithmatic.py
+++ arithmatic.py
@@ -3,8 +3,16 @@ def addition(a,b):
     print(ans)
     return ans
 
+# code added by swapnil
+def subtraction(a,b):
+    ans=a-b
+    print(ans)
+    return ans
+
 
 x=10
 y=20
 
 print(f"{addition(x,y)}")
+print(f"{subtraction(x,y)}")
+
